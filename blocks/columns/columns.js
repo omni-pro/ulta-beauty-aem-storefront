49514712d3009fa8d8c08df189f8de50e16d593f
Oscar Salas <oscar.salas@omni.pro>
2025-03-21 11:05:43 -0500

    columns example

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
-
+  console.log('Block Columns:', block);
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
